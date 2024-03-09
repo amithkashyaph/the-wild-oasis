7bd614b4b6eac37aabfdf92ba7e0a9aa07935144
amithkashyap <amithkashyaprv@gmail.com>
2024-03-09 21:23:04 +0530

    added style component to AppLayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,19 +5,26 @@ import Sidebar from "./Sidebar";
 import styled from "styled-components";
 
 const Main = styled.main`
-  background-color: green;
+  background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
+`;
+
+const StyledAppLayout = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
 `;
 
 const AppLayout = () => {
   return (
-    <div>
+    <StyledAppLayout>
       <Header />
       <Sidebar />
       <Main>
         <Outlet />
       </Main>
-    </div>
+    </StyledAppLayout>
   );
 };
 
