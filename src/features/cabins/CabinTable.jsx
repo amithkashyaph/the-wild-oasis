f3a46f597ed0b4d48f7a42d02dca099cfd23f2d9
amithkashyap <amithkashyaprv@gmail.com>
2024-03-16 15:56:53 +0530

    refactored CabinTable component to fetch data using React Query

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
 import { getCabins } from "../../services/apiCabins";
 import Spinner from "../../ui/Spinner";
+import CabinRow from "./CabinRow";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -40,7 +41,21 @@ const CabinTable = () => {
   if (isLoading) {
     return <Spinner />;
   }
-  return <div>Cabin Table</div>;
+  return (
+    <Table role="table">
+      <TableHeader role="row">
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+      {cabins.map((cabin) => (
+        <CabinRow cabin={cabin} key={cabin.id} />
+      ))}
+    </Table>
+  );
 };
 
 export default CabinTable;
