71ed7cc861673566ac654deb69bf78ce3fd7522b
amithkashyap <amithkashyaprv@gmail.com>
2024-03-16 22:13:31 +0530

    added mutation from ReactQuery hook to add Cabin via createCabin API

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -6,6 +6,9 @@ import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
 import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const FormRow = styled.div`
   display: grid;
@@ -44,10 +47,24 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
+  const queryClient = useQueryClient();
+  const { mutate, isLoading: isCreating } = useMutation({
+    mutationFn: (cabinData) => createCabin(cabinData),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+      toast.success("Cabin created successfully");
+      reset();
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = (cabinData) => {
+    mutate(cabinData);
   };
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
@@ -88,7 +105,7 @@ function CreateCabinForm() {
 
       <FormRow>
         <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" {...register("image")} />
+        <FileInput id="image" accept="image/*" />
       </FormRow>
 
       <FormRow>
@@ -96,7 +113,7 @@ function CreateCabinForm() {
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
-        <Button>Add cabin</Button>
+        <Button disabled={isCreating}>Add cabin</Button>
       </FormRow>
     </Form>
   );
