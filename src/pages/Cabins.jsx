dec993a58ca4958224ffc896eca69c236bbbc566
amithkashyap <amithkashyaprv@gmail.com>
2024-03-18 00:48:27 +0530

    added Create Cabin form and a button to toggle its display

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import Button from "../ui/Button";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   useEffect(() => {
     getCabins().then((data) => console.log(data));
   }, []);
@@ -17,6 +20,10 @@ function Cabins() {
       <Row>
         <CabinTable />
       </Row>
+      <Button onClick={() => setShowForm((showForm) => !showForm)}>
+        Show form
+      </Button>
+      {showForm && <CreateCabinForm />}
     </>
   );
 }
