8479744353be8919b0d2e0e08fb291896887e5d6
amithkashyap <amithkashyaprv@gmail.com>
2024-03-18 00:47:30 +0530

    added option to edit existing cabin

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
+import { useState } from "react";
+import CreateCabinForm from "./CreateCabinForm";
 
 const TableRow = styled.div`
   display: grid;
@@ -43,6 +46,8 @@ const Discount = styled.div`
 `;
 
 const CabinRow = ({ cabin }) => {
+  const [showEditForm, setShowEditForm] = useState(false);
+
   const {
     id: cabinId,
     name,
@@ -60,19 +65,37 @@ const CabinRow = ({ cabin }) => {
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
+      toast.success("Cabin successfully deleted");
     },
+    onError: (error) => toast.error(error.message),
   });
   return (
-    <TableRow role="row">
-      <Img src={image} />
-      <Cabin>{name}</Cabin>
-      <div>Fits upto {maxCapacity} guests</div>
-      <Price>{formatCurrency(regularPrice)}</Price>
-      <Discount>{formatCurrency(discount)}</Discount>
-      <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
-        Delete
-      </button>
-    </TableRow>
+    <>
+      <TableRow role="row">
+        <Img src={image} />
+        <Cabin>{name}</Cabin>
+        <div>Fits upto {maxCapacity} guests</div>
+        <Price>{formatCurrency(regularPrice)}</Price>
+        <Discount>{formatCurrency(discount)}</Discount>
+        <div>
+          <button
+            onClick={() => setShowEditForm((showForm) => !showForm)}
+            disabled={isDeleting}
+          >
+            Edit
+          </button>
+          <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
+            Delete
+          </button>
+        </div>
+      </TableRow>
+      {showEditForm && (
+        <CreateCabinForm
+          editCabinData={cabin}
+          onClose={() => setShowEditForm(false)}
+        />
+      )}
+    </>
   );
 };
 
