c82d7819764baa1f859677142e8b268fae91fbbc
amithkashyap <amithkashyaprv@gmail.com>
2024-03-18 23:40:30 +0530

    added Edit capability within Create API based on id param

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   let { data, error } = await supabase.from("cabins").select("*");
@@ -6,6 +6,50 @@ export async function getCabins() {
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+
+export async function createEditCabin(newCabin, id) {
+  console.log("newCabin : ", newCabin);
+  console.log("id : ", id);
+  const hasImagePath = newCabin?.image?.startsWith?.(supabaseUrl);
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+
+  // Sample image path : https://pnfwpqlexwejpbkhznjm.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2024-03-10T11%3A41%3A55.992Z
+  const imagePath = hasImagePath
+    ? newCabin.image
+    : `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  let query = supabase.from("cabins");
+
+  // Step 1 : Create Cabin if ID is not present
+  if (!id) {
+    query = query.insert([{ ...newCabin, image: imagePath }]);
+  } else {
+    query = query.update({ ...newCabin, image: imagePath }).eq("id", id);
+  }
+  const { data, error } = await query.select().single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be created");
+  }
+
+  // Step 2 : Upload file to the storage bucket
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  // Step 3 : If there was any error in uploading the image file to bucket then delete the Cabin entry in the table
+  if (storageError) {
+    deleteCabin(data.id);
+    console.error(storageError);
+    throw new Error("Cabin could not be created due to file upload error");
   }
 
   return data;
