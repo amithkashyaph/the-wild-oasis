1d024032d818e66c1c05f3acfbd8ac3ab7aea110
amithkashyap <amithkashyaprv@gmail.com>
2024-03-24 11:45:45 +0530

    implemented Cabin Table component using Compound Component pattern

---
+++ src/features/cabins/CabinTableCompoundComponent.jsx
@@ -0,0 +1,28 @@
+import Spinner from "../../ui/Spinner";
+import CabinRow from "./CabinRow";
+import { useCabins } from "./useCabins";
+import { Table } from "../../ui/Table";
+
+const CabinTableCompoundComponent = () => {
+  const { isLoading, cabins } = useCabins();
+  if (isLoading) {
+    return <Spinner />;
+  }
+  return (
+    <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+      <Table.Header>
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </Table.Header>
+      {cabins.map((cabin) => (
+        <CabinRow cabin={cabin} key={cabin.id} />
+      ))}
+    </Table>
+  );
+};
+
+export default CabinTableCompoundComponent;
