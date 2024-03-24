058feacf1fe91a1fa3f23499ca46ff36e0d08e8b
amithkashyap <amithkashyaprv@gmail.com>
2024-03-24 16:21:10 +0530

    added onChange handler to set the query param in the url

--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -1,7 +1,14 @@
 import Select from "./Select";
+import { useSearchParams } from "react-router-dom";
 
 const SortBy = ({ options }) => {
-  return <Select options={options} type="white" />;
+  const [searchParam, setSearchParam] = useSearchParams();
+
+  const handleChange = (e) => {
+    searchParam.set("sortBy", e.target.value);
+    setSearchParam(searchParam);
+  };
+  return <Select options={options} type="white" onChange={handleChange} />;
 };
 
 export default SortBy;
