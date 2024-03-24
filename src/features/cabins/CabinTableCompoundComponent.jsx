c984396c024daa43b20654b9764a072cc8b65d19
amithkashyap <amithkashyaprv@gmail.com>
2024-03-24 16:56:23 +0530

    added sorting logic to the Cabins table based on field and direction

--- src/features/cabins/CabinTableCompoundComponent.jsx
+++ src/features/cabins/CabinTableCompoundComponent.jsx
@@ -1,7 +1,7 @@
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRowCompoundComponent";
 import { useCabins } from "./useCabins";
-import { Table } from "../../ui/Table";
+import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
 
@@ -13,6 +13,9 @@ const CabinTableCompoundComponent = () => {
     return <Spinner />;
   }
 
+  /**
+   * 1. Filter based on discount
+   */
   const filterValue = searchParams.get("discount") || "all";
   let filteredCabins;
 
@@ -22,6 +25,20 @@ const CabinTableCompoundComponent = () => {
 
   if (filterValue === "with-discount")
     filteredCabins = cabins.filter((c) => c.discount > 0);
+
+  /**
+   * 2. Sort based on fields in the url
+   */
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) =>
+      (typeof a[field] !== "string"
+        ? a[field] - b[field]
+        : a[field].localeCompare(b[field])) * modifier
+  );
+
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -34,7 +51,7 @@ const CabinTableCompoundComponent = () => {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
