31cb1a033c5682901c265590ab876a76fb6f9fa2
amithkashyap <amithkashyaprv@gmail.com>
2024-04-01 02:15:18 +0530

    added pagination buttons and chevron icons

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -1,3 +1,4 @@
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 import styled from "styled-components";
 
 const StyledPagination = styled.div`
@@ -62,6 +63,15 @@ const Pagination = () => {
       <P>
         Showing <span>1</span> to <span>10</span> of <span>23</span> results
       </P>
+      <Buttons>
+        <PaginationButton>
+          <HiChevronLeft /> <span>Previous</span>
+        </PaginationButton>
+        <PaginationButton>
+          <span>Next</span>
+          <HiChevronRight />
+        </PaginationButton>
+      </Buttons>
     </StyledPagination>
   );
 };
