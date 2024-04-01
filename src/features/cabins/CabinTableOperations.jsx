831a68b8648e7df061a3b11f15106dcc92449a5a
amithkashyap <amithkashyaprv@gmail.com>
2024-04-02 01:02:43 +0530

    added options to sortBy in Cabin table

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,4 +1,5 @@
 import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
 import TableOperations from "../../ui/TableOperations";
 
 const CabinTableOperations = () => {
@@ -18,6 +19,34 @@ const CabinTableOperations = () => {
           {
             label: "With discount",
             value: "with-discount",
+          },
+        ]}
+      />
+      <SortBy
+        options={[
+          {
+            label: "Sort by name [A-Z]",
+            value: "name-asc",
+          },
+          {
+            value: "name-desc",
+            label: "Sort by name [Z-A]",
+          },
+          {
+            value: "regularPrice-asc",
+            label: "Sort by price [low-high]",
+          },
+          {
+            value: "regularPrice-desc",
+            label: "Sort by price [high-low]",
+          },
+          {
+            value: "maxCapacity-asc",
+            label: "Sort by maxCapacity [low-high]",
+          },
+          {
+            value: "maxCapacity-desc",
+            label: "Sort by maxCapacity [high-low]",
           },
         ]}
       />
