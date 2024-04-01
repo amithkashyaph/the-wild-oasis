9980852d876947f7373690edafc8857a0594f3e5
amithkashyap <amithkashyaprv@gmail.com>
2024-04-02 02:54:06 +0530

    added logic to support pagination with React query for Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -5,6 +5,7 @@ import { useSearchParams } from "react-router-dom";
 export function useBookings() {
   const [searchParams] = useSearchParams();
 
+  // Read Filter value from query param
   const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
@@ -14,17 +15,21 @@ export function useBookings() {
           value: filterValue,
         };
 
+  // Read sortBy value from query param
   const sortByRaw = searchParams.get("sortBy") || "startDate-asc";
   const [field, direction] = sortByRaw.split("-");
   const sortBy = { field, direction };
 
+  // Read the page value from query param
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
   const {
     isLoading,
-    data: { data: bookings, count },
+    data: { data: bookings, count } = {},
     error,
   } = useQuery({
-    queryKey: ["bookings", filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
   return { isLoading, bookings, error, count };
