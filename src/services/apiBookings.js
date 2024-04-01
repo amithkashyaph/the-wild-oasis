a171c4beac2e6e7dd52b35d5f0c5189d28cf6cd8
amithkashyap <amithkashyaprv@gmail.com>
2024-04-02 02:41:32 +0530

    added count field in the API query

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -5,7 +5,10 @@ export async function getBookings({ filter, sortBy }) {
   let query = supabase
     .from("bookings")
     .select(
-      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName,email)"
+      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName,email)",
+      {
+        count: "exact",
+      }
     );
 
   // Filter
@@ -20,14 +23,14 @@ export async function getBookings({ filter, sortBy }) {
     });
   }
 
-  const { data, error } = await query;
+  const { data, error, count } = await query;
 
   if (error) {
     console.error(error);
     throw new Error("Bookinngs could not be loaded");
   }
 
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {
