6ce8d208b32cb6c05b581e686644b49b2689369d
amithkashyap <amithkashyaprv@gmail.com>
2024-04-05 00:17:44 +0530

    added Cabin table operations component on Cabins page

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,13 +2,14 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { AddCabin } from "../features/cabins/AddCabin";
 import CabinTableCompoundComponent from "../features/cabins/CabinTableCompoundComponent";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
 function Cabins() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Sort / Filter</p>
+        <CabinTableOperations />
       </Row>
       <Row>
         <CabinTableCompoundComponent />
