be049f189da1ab9fc94eff994eb8430b05bb51af
amithkashyap <amithkashyaprv@gmail.com>
2024-04-06 01:03:00 +0530

    on login error reset the login form

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -19,7 +19,15 @@ function LoginForm() {
       return;
     }
 
-    login({ email, password });
+    login(
+      { email, password },
+      {
+        onError: () => {
+          setEmail("");
+          setPassword("");
+        },
+      }
+    );
   }
 
   return (
