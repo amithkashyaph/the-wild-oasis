fae5201b899ffbfb8b34988abe6c3fb78c24dc5e
amithkashyap <amithkashyaprv@gmail.com>
2024-04-06 01:08:40 +0530

    stored the user data in local cache to avoid network call

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,15 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginAPI } from "../../services/apiAuth";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const { mutate: login, isLoading: isLoggingIn } = useMutation({
     mutationFn: ({ email, password }) => loginAPI({ email, password }),
-    onSuccess: () => {
+    onSuccess: (user) => {
       toast.success("Successfully logged in");
-      navigate("/dashboard");
+      queryClient.setQueryData(["user"], user);
+      navigate("/dashboard", { replace: true });
     },
     onError: () =>
       toast.error(
