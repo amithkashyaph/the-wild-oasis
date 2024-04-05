02fbc5387cb53468073920c11c666d6d756b879c
amithkashyap <amithkashyaprv@gmail.com>
2024-04-06 01:03:54 +0530

    integrated logout functionality on click of logout button

--- src/features/authentication/Logout.jsx
+++ src/features/authentication/Logout.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ButtonIcon from "../../ui/ButtonIcon";
 import { HiArrowRightOnRectangle } from "react-icons/hi2";
+import { useLogout } from "./useLogout";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 const Logout = () => {
+  const { logout, isLoggingout } = useLogout();
   return (
-    <ButtonIcon>
-      <HiArrowRightOnRectangle />
+    <ButtonIcon disabled={isLoggingout} onClick={logout}>
+      {!isLoggingout ? <HiArrowRightOnRectangle /> : <SpinnerMini />}
     </ButtonIcon>
   );
 };
