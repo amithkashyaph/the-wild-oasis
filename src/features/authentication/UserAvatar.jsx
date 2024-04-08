dfe0e5f7d816209c22136cc1d42579b73062f029
amithkashyap <amithkashyaprv@gmail.com>
2024-04-08 22:19:52 +0530

    implemented UserAvatar component

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useUser } from "./useUser";
 
 const StyledUserAvatar = styled.div`
   display: flex;
@@ -21,7 +22,18 @@ const Avatar = styled.img`
 `;
 
 const UserAvatar = () => {
-  return <div>UserAvatar</div>;
+  const { user } = useUser();
+  const { fullName, avatar } = user.user_metadata;
+
+  return (
+    <StyledUserAvatar>
+      <Avatar
+        src={avatar || "default-user.jpg"}
+        alt={`Avatar of user ${fullName}`}
+      />
+      <span>{fullName}</span>
+    </StyledUserAvatar>
+  );
 };
 
 export default UserAvatar;
