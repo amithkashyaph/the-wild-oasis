f9ade750460f7be35aea4f07f5c0420bea193fe2
amithkashyap <amithkashyaprv@gmail.com>
2024-04-20 07:22:44 +0530

    minor refactor

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -91,7 +91,10 @@ const SalesChart = ({ bookings, numDays }) => {
       };
   return (
     <StyledSalesChart>
-      <Heading as="h2">Sales</Heading>
+      <Heading as="h2">
+        Sales from {formatDate(allDates.at(0), "MMM dd yyyy")} &mdash;{" "}
+        {formatDate(allDates.at(-1), "MMM dd yyyy")}
+      </Heading>
       <ResponsiveContainer width="100%" height={300}>
         <AreaChart data={data}>
           <XAxis
