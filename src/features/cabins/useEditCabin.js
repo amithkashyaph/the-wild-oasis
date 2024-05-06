6507df9299b7beb88e5b78d4125fbdde3a7f5bbd
amithkashyap <amithkashyaprv@gmail.com>
2024-05-07 03:12:10 +0530

    implemented the edit cabin hook

---
+++ src/features/cabins/useEditCabin.js
@@ -0,0 +1,18 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { createEditCabin } from "../../services/apiCabins";
+
+export function useEditCabin() {
+  const queryClient = useQueryClient();
+
+  const { mutate: editCabin, isLoading: isEditing } = useMutation({
+    mutationFn: ({ newCabin, id }) => createEditCabin(newCabin, id),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["cabins"]);
+      toast.success("Cabin edited successfully");
+    },
+    onError: (error) => toast.error(error.message),
+  });
+
+  return { isEditing, editCabin };
+}
