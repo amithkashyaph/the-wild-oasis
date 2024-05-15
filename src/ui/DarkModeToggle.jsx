60515b6cc86baa2966f9ed1e2892692b69ff7f20
amithkashyap <amithkashyaprv@gmail.com>
2024-05-16 02:20:04 +0530

    added dark mode toggle component

--- src/ui/DarkModeToggle.jsx
+++ src/ui/DarkModeToggle.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import ButtonIcon from "./ButtonIcon";
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi2";
+import { useDarkMode } from "../context/DarkModeContext";
 
 const DarkModeToggle = () => {
-  return <div>DarkModeToggle</div>;
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
+  return (
+    <ButtonIcon onClick={toggleDarkMode}>
+      {isDarkMode ? <HiOutlineSun /> : <HiOutlineMoon />}
+    </ButtonIcon>
+  );
 };
 
 export default DarkModeToggle;
