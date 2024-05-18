85d4dbd3257694ad4e54c7901db3d0653844bccb
amithkashyap <amithkashyaprv@gmail.com>
2024-05-19 01:06:47 +0530

    refactored sortBy component

--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -3,12 +3,20 @@ import { useSearchParams } from "react-router-dom";
 
 const SortBy = ({ options }) => {
   const [searchParam, setSearchParam] = useSearchParams();
+  const currentSortParam = searchParam.get("sortBy") || "";
 
   const handleChange = (e) => {
     searchParam.set("sortBy", e.target.value);
     setSearchParam(searchParam);
   };
-  return <Select options={options} type="white" onChange={handleChange} />;
+  return (
+    <Select
+      options={options}
+      type="white"
+      onChange={handleChange}
+      value={currentSortParam}
+    />
+  );
 };
 
 export default SortBy;
