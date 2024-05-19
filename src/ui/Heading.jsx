a8e07d88718e9b22262b0b706d1f4131150db6c3
amithkashyap <amithkashyaprv@gmail.com>
2024-05-19 11:59:51 +0530

    tweaked Heading component

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,9 +1,32 @@
 import styled from "styled-components";
 
 const Heading = styled.h1`
-  font-size: 20px;
+  ${(props) =>
+    props.as === "h1" &&
+    `font-size: 3rem;
   font-weight: 600;
-  background-color: yellow;
+  `}
+
+  ${(props) =>
+    props.as === "h2" &&
+    `font-size: 2rem;
+  font-weight: 600;
+  `}
+
+  ${(props) =>
+    props.as === "h3" &&
+    `font-size: 2rem;
+  font-weight: 500;
+  `}
+
+${(props) =>
+    props.as === "h4" &&
+    `font-size: 3rem;
+  font-weight: 600;
+  text-align: center;
+  `}
+  
+  line-height: 1.4rem;
 `;
 
 export default Heading;
